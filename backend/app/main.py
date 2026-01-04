e84bae222b9e2593187142ea5f9c3568fa138fe5
Nimsi Dharmaratne <nimsidinara@gmail.com>
2026-01-04 18:18:45 +0530

    Add emotion analysis router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database.mongodb import users_collection
 from app.routes import videos
+from app.routes import emotions
 
 app = FastAPI(title="DanceForge AI API")
 
@@ -16,6 +17,7 @@ app.add_middleware(
 
 # Include routers
 app.include_router(videos.router)
+app.include_router(emotions.router)
 
 @app.get("/")
 def test_db():
